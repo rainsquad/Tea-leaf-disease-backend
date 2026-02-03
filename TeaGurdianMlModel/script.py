db3be9cd98e6ac95e7a1edf2649b43e6c1b2a36c
rainsquad <charukaganganath@gmail.com>
2026-02-03 17:49:47 +0530

    Port issue

--- TeaGurdianMlModel/script.py
+++ TeaGurdianMlModel/script.py
@@ -76,4 +76,5 @@ def predict():
 
 # ---------------- Run App ---------------- #
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
